c21f18d380f2a5339bfd73dfd1ebd9c62e57fb19
Emreman5 <emre15087@gmail.com>
2022-01-20 15:04:56 +0300

    Translator ENG-TR

--- functions/translator.py
+++ functions/translator.py
@@ -1,6 +1,6 @@
 import googletrans
 from googletrans import Translator
-import pandas as pd# Desteklenen dil sayısı
+import pandas as pd
 def get_translate(string):
     translator = Translator()
     return translator.translate(string, dest = "tr").text
