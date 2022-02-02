0eb8a42c61fae07fbac29af3c6c29a33caf9f4db
Emreman5 <emre15087@gmail.com>
2022-02-02 19:13:37 +0300

    Database commands

--- launcher.py
+++ launcher.py
@@ -1,12 +1,18 @@
 from unittest import async_case
-from discord.ext import commands
+from discord.ext import commands,tasks
 from settings import *
 import os
 from discord.utils import find
+import discord
+bot = commands.Bot(command_prefix='',activity = discord.Game(name="Duygularınla"))
 
-bot = commands.Bot(command_prefix='!')
-LOCK = 1
 
+@bot.event
+async def on_guild_join(guild):
+    general = find(lambda x: x.name == 'genel',  guild.text_channels)
+    if general and general.permissions_for(guild.me).send_messages:
+        await general.send(f'**SAKİN BEYLER ENGİZ DOLMUŞ GELDİ!**')
+        await general.send("@everyone")
 
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py") and filename != "__init__.py":
